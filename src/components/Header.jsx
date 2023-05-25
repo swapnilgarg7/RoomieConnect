7ab48ff5c5afa2469e4cc4ac0e1388b7df531659
swapnilgarg7 <swapnilgarg810@gmail.com>
2023-05-26 00:39:38 +0300

    navbar functional

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 function Header() {
     const location = useLocation();
+    const navigate = useNavigate();
 
     function pathMatchRoute(route) {
         return location.pathname === route ? true : false
@@ -10,18 +11,23 @@ function Header() {
     return (
         <div className='bg-white border-b shadow-sm sticky top-0 z-50'>
             <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
-                <div className='flex items-center py-1'>
+                <div className='flex items-center py-1 cursor-pointer'
+                    onClick={() => navigate("/")}>
                     <img src='/logo.png' alt='logo' className='w-[50px] h-[50px]' />
                     <h1 className='text-3xl text-primary font-'>Roomie Connect</h1>
                 </div>
                 <div>
                     <ul className='flex space-x-10 text-sm font-bold text-secondary'>
                         <li className={`cursor-pointer py-3 border-b-[3px] border-transparent
-                        ${pathMatchRoute("/") && "text-primary border-b-accent"}`}>Home</li>
+                        ${pathMatchRoute("/") && "text-primary border-b-accent"}`
+                        }
+                            onClick={() => navigate("/")}>Home</li>
                         <li className={`cursor-pointer py-3 border-b-[3px] border-transparent
-                        ${pathMatchRoute("/offers") && "text-primary border-b-accent"}`}>Offers</li>
+                        ${pathMatchRoute("/offers") && "text-primary border-b-accent"}`}
+                            onClick={() => navigate("/offers")}>Offers</li>
                         <li className={`cursor-pointer py-3 border-b-[3px] border-transparent
-                        ${pathMatchRoute("/login") && "text-primary border-b-accent"}`}>Login</li>
+                        ${pathMatchRoute("/login") && "text-primary border-b-accent"}`}
+                            onClick={() => navigate("/login")}>Login</li>
                     </ul>
                 </div>
             </header>
